acbeae60b2075768157bd2f9bb6bf8587f62f0e6
AMikail0402 <alpay.yildiz@outlook.com>
2023-10-18 22:40:52 +0200

    Anpassung an IDS

--- src/api/Services.ts
+++ src/api/Services.ts
@@ -3,12 +3,12 @@ const pool = require("../db")
 
    function create(data, callBack){
         console.log("Daten"+data);
-        const name: string = "'"+data.name+"'";
-        const adress: string = "'"+data.adress+"'";
-        const gender: string = "'"+data.gender+"'";
-        console.log("Name"+name+"\nAdresse"+adress+"\nGeschlecht"+gender) 
-        console.log("SQL-Befehl: "+'insert into example_table (name, adress, gender) values ('+name+','+adress+','+gender+')')
-        const query: string = 'insert into example_table (name, adress, gender) values ('+name+','+adress+','+gender+')';
+        const cve: string = "'"+data.cve+"'";
+        const msg: string = "'"+data.msg+"'";
+        const time: string = "'"+data.time+"'";
+        console.log("CVE"+cve+"\nNachricht"+msg+"\nZeit"+time) 
+        console.log("SQL-Befehl: "+'insert into example_table (name, adress, gender) values ('+cve+','+msg+','+time+')')
+        const query: string = 'insert into example_table (cve, msg, time) values ('+cve+','+msg+','+time+')';
         pool.query(query, callBack);
         
     } 
