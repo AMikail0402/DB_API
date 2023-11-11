7e0c9296e41533dfd7ec8e1e5da92098e83f136e
AMikail0402 <alpay.yildiz@outlook.com>
2023-11-11 16:16:25 +0100

    UI-Fortschritt

--- apps/Api-UI/src/App.ts
+++
@@ -1,9 +0,0 @@
-import { dashboardRouter } from '../../Api-Server/src/api/Router';
-import express from 'express';
-
-const app = express()
-
-app.use(express.json());
-
-app.use("/dashboard", dashboardRouter)
-
