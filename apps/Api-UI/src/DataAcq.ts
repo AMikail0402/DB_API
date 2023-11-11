9bafde49c6980736a0a3031f1f1962e7b6441644
AMikail0402 <alpay.yildiz@outlook.com>
2023-11-11 16:24:33 +0100

    Response

---
+++ apps/Api-UI/src/DataAcq.ts
@@ -0,0 +1,45 @@
+
+type Entry = {
+  cve: string;
+  msg: string;
+  time: string;
+  src_address: string;
+};
+
+type Response = {
+  data: Entry[];
+};
+
+async function getEntries() {
+  try {
+    // 👇️ const response: Response
+    const response = await fetch('http://localhost:3000/api/users', {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error! status: ${response.status}`);
+    }
+
+    // 👇️ const result: GetUsersResponse
+    const result = await (response.json()) as Response
+
+    console.log('result is: ', JSON.stringify(result, null, 4));
+
+    return result;
+
+  } catch (error) {
+    if (error instanceof Error) {
+      console.log('error message: ', error.message);
+      return error.message;
+    } else {
+      console.log('unexpected error: ', error);
+      return 'An unexpected error occurred';
+    }
+  }
+}
+
+getEntries();
