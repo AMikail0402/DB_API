7d1bdf3358e8c648d331d4ccc564e4483aa6821c
AMikail0402 <alpay.yildiz@outlook.com>
2023-12-31 01:24:08 +0100

    Microfix

--- src/api/Services.ts
+++ src/api/Services.ts
@@ -6,7 +6,7 @@ import { client }  from '../Db/Db';
         const cve: string = "'"+data.cve+"'";
         const msg: string = "'"+data.msg+"'";
         const time: string = "'"+data.time+"'";
-        const src_address: string = "'"+data.src_address+"'";
+        const src_address: string = "'"+data.src_address.replace("\/","")+"'";
         console.log("CVE"+cve+"\nNachricht"+msg+"\nZeit"+time) 
         console.log("SQL-Befehl: "+'insert into example_table (cve, msg, time, src_address) values ('+cve+','+msg+','+time+','+src_address+')');
         const query: string = 'insert into example_table (cve, msg, time, src_address) values ('+cve+','+msg+','+time+','+src_address+')';
